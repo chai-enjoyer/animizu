77c7b49783213cd1c5f4b6f30c8a3cb2428a5361
Batyrkhan Tursumbekov <tumarbatr@gmail.com>
2024-11-19 06:31:21 +0500

    Added title pages, updated images

--- js/randomBackground.js
+++ js/randomBackground.js
@@ -1,23 +1,27 @@
-// List of images for the background
-const backgrounds = [
-    '../images/Bleach/tybw_1.png',
-    '../images/Bleach/tybw_2.png',
-    '../images/Bleach/tybw_3.png',
-    '../images/Bleach/tybw_4.png',
-    '../images/Bleach/tybw_5.png',
-    '../images/Bleach/tybw_6.png'
-];
+window.addEventListener('load', function() {
+    function changeBackground() {
 
-// Function to change background randomly
-function changeBackground() {
-    const randomIndex = Math.floor(Math.random() * backgrounds.length);
-    const featuredContent = document.querySelector('.featured-content');
-    featuredContent.style.backgroundImage = `linear-gradient(to bottom, rgba(0, 0, 0, 0), #151515), url(${backgrounds[randomIndex]})`;
-}
+        const title = document.body.className;
 
-// Change background every 5 seconds
-setInterval(changeBackground, 5000);
+        const backgrounds = [
+            `../images/${title}/bg_1.png`,
+            `../images/${title}/bg_2.png`,
+            `../images/${title}/bg_3.png`,
+            `../images/${title}/bg_4.png`,
+            `../images/${title}/bg_5.png`,
+            `../images/${title}/bg_6.png`
+        ];
 
-// Initial background change on page load
-changeBackground();
+        const randomIndex = Math.floor(Math.random() * backgrounds.length);
+
+        const featuredContent = document.querySelector('.featured-content');
+        featuredContent.style.transition = 'background-image 1s ease-in-out';
+        featuredContent.style.background = `linear-gradient(to bottom, rgba(0, 0, 0, 0), #151515), url(${backgrounds[randomIndex]})`;
+    }
+
+
+    setInterval(changeBackground, 5000);
+
+    changeBackground();
+});
 
